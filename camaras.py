1362aea8ba6aea58ebdddd1f4b5c72aa37f7f3c4
Eduardo <eduardopoblete1998@gmail.com>
2026-01-26 18:49:26 -0300

    Added camaras.py

---
+++ camaras.py
@@ -0,0 +1,43 @@
+import locale
+locale.setlocale(locale.LC_NUMERIC, 'C')
+
+import sys
+import mpv
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QFrame
+
+CAMARAS = [
+    "rtsp://user:pass@YOUR-IP/h264/ch1/main/av_stream",
+]
+
+class Player(QFrame):
+    def __init__(self, url):
+        super().__init__()
+        self.setStyleSheet("background-color: black;")
+
+        self.player = mpv.MPV(
+            wid=str(int(self.winId())),
+            vo='gpu',
+            hwdec='auto',
+            rtsp_transport='tcp',
+            audio='no'
+        )
+        self.player.play(url)
+    
+class CamarasApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Mis Cámaras")
+        self.setGeometry(100, 100, 1000, 700)
+
+        layout = QGridLayout(self)
+        posiciones = [(0,0), (0,1), (1,0), (1,1)]
+
+        for pos, url in zip(posiciones, CAMARAS):
+            cam = Player(url)
+            layout.addWidget(cam, *pos)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = CamarasApp()
+    win.show()
+    sys.exit(app.exec_())
